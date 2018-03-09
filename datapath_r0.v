77a473bc90811c8d9be272dddd3541a812294378
Nicholas Kroft <stangs56@gmail.com>
2018-03-08 19:50:50 -0500

    slightly more working less broken

--- datapath_r0.v
+++ datapath_r0.v
@@ -9,7 +9,7 @@ TODO
 
 module datapath_r0 #(
 	parameter BIT_WIDTH = 32,
-	parameter ADDR_WIDTH = 32,	
+	parameter ADDR_WIDTH = 32,
 	parameter DELAY = 0
 )(
 	input  clk,
@@ -18,7 +18,7 @@ module datapath_r0 #(
 );
 
 /**********
-*  Array Packing Defines 
+*  Array Packing Defines
 **********/
 //These are preprocessor defines similar to C/C++ preprocessor or VHDL functions
 	`define PACK_ARRAY(PK_WIDTH,PK_DEPTH,PK_SRC,PK_DEST, BLOCK_ID, GEN_VAR)    genvar GEN_VAR; generate for (GEN_VAR=0; GEN_VAR<(PK_DEPTH); GEN_VAR=GEN_VAR+1) begin: BLOCK_ID assign PK_DEST[((PK_WIDTH)*GEN_VAR+((PK_WIDTH)-1)):((PK_WIDTH)*GEN_VAR)] = PK_SRC[GEN_VAR][((PK_WIDTH)-1):0]; end endgenerate
@@ -38,146 +38,197 @@ module datapath_r0 #(
 	parameter SHAMT_WIDTH = 5;
 	parameter IMM_WIDTH = 16;
 	parameter STATUS_WIDTH = 4;
-	
+
 	//clocks
 	wire clk_sys;
 	wire clk_mem;
-	
+
 	//control signals from controller
-	
+	//note: all in ID stage
+	//delayed versions created as needed
+
 	wire [ALUOP_WIDTH-1:0] ALUop;
-	
+
 	wire regWrite;
 	wire regDest;
 	wire memToReg;
-	
+
 	wire load_upper;
 	wire isSigned;
 	wire ALUsrc;
-	
+
 	wire jump;
 	wire jal;
 	wire branch;
 	wire eq;
-	
+
 	wire memRead;
 	wire memWrite;
-	
+
 	wire memIsSigned;
 	wire [1:0] memDataSize;
-	
+
 	wire [ALUOP_WIDTH+9-1:0] combined;
-	
+
 	wire jr;
-	
+
 	//control signals created in datapath
 	wire branchTaken;
-	wire C, Z, S, V;
-	
+
+
+/**********
+ * Fetch
+ **********/
+
 	//PC
 	wire [ADDR_WIDTH-1:0] PC_out;
 	wire [ADDR_WIDTH-1:0] PC_in;
 	wire [ADDR_WIDTH-1:0] PC_plus4;
-	wire [ADDR_WIDTH-1:0] PC_branchTmp;
-	
-	wire [ADDR_WIDTH-1:0] PC_branchMux;
-	
-	wire PC_plus4_ovf, PC_branchTmp_ovf;
-	
-	reg  [ADDR_WIDTH-1:0] PC; //replace with PC component
-	
-	//instruction split
-	wire [BIT_WIDTH-1:0] instruction;
-	
-	wire [OP_WIDTH-1:0] opcode;
-	wire [REG_ADDR_WIDTH-1:0] rs, rt, rd;
-	wire [IMM_WIDTH-1:0] immediate;
-	wire [SHAMT_WIDTH-1:0] shamt;
-	wire [FUNCT_WIDTH-1:0] funct;
-	wire [BIT_WIDTH-OP_WIDTH-1:0] jConst;
-	
-	//registers
-	wire [REG_ADDR_WIDTH-1:0] regToWrite;
-	wire [BIT_WIDTH-1:0] regWriteData;
-	wire [REG_ADDR_WIDTH-1:0] regRead [1:0];
-	wire [2*REG_ADDR_WIDTH-1:0] rr_tmp;
-	wire [2*BIT_WIDTH-1:0] q_tmp;
-	wire [BIT_WIDTH-1:0] readData [1:0];
-	
-	//sign extender
-	wire [BIT_WIDTH-1:0] imm_extended;
-	
-	//ALU
-	wire [ALUFUNCT_WIDTH-1:0] ALUfunct;
-	wire [STATUS_WIDTH-1:0] status;
-	wire [BIT_WIDTH-1:0] ALUDataIn [1:0];
-	wire [2*BIT_WIDTH-1:0] ALUDataIn_tmp;
-	wire [BIT_WIDTH-1:0] ALUDataOut;
-	
-	//writeback
-	wire [BIT_WIDTH-1:0] dataMemOut;
-	wire [BIT_WIDTH-1:0] wbOut;
+	wire PC_plus4_ovf;
 
 /**********
- * Glue Logic 
+ * Decode
  **********/
- 
+
+ //Branch
+
+  wire [ADDR_WIDTH-1:0] PC_branchTmp;
+
+  wire [ADDR_WIDTH-1:0] PC_branchMux;
+
+  wire PC_branchTmp_ovf;
+	wire id_reg_eq;
+
+	//instruction split
+	wire [BIT_WIDTH-1:0] id_instruction;
+
+	wire [OP_WIDTH-1:0] id_opcode;
+	wire [REG_ADDR_WIDTH-1:0] id_rs, id_rt, id_rd;
+	wire [IMM_WIDTH-1:0] id_immediate;
+	wire [SHAMT_WIDTH-1:0] id_shamt;
+	wire [FUNCT_WIDTH-1:0] id_funct;
+	wire [BIT_WIDTH-OP_WIDTH-1:0] id_jConst;
+
+	//registers
+	wire [REG_ADDR_WIDTH-1:0] id_regToWrite;
+	wire [REG_ADDR_WIDTH-1:0] id_regRead [1:0];
+	wire [2*REG_ADDR_WIDTH-1:0] id_rr_tmp;
+	wire [2*BIT_WIDTH-1:0] id_q_tmp;
+	wire [BIT_WIDTH-1:0] id_readData [1:0];
+	wire [BIT_WIDTH-1:0] id_regWriteData;
+
+	//sign extender
+	wire [BIT_WIDTH-1:0] id_imm_extended;
+
+/**********
+ * Execute
+ **********/
+	//registers
+	wire [BIT_WIDTH-1:0] ex_readData [1:0];
+
+  //control
+  wire [FUNCT_WIDTH-1:0] ex_funct;
+	wire [SHAMT_WIDTH-1:0] ex_shamt;
+  wire [ALUOP_WIDTH-1:0] ex_ALUop;
+	wire ex_ALUsrc;
+
+	//ALU
+	wire [ALUFUNCT_WIDTH-1:0] ex_ALUfunct;
+	wire [BIT_WIDTH-1:0] ex_ALUDataIn [1:0];
+	wire [2*BIT_WIDTH-1:0] ex_ALUDataIn_tmp;
+
+	//sign extender
+	wire [BIT_WIDTH-1:0] ex_imm_extended;
+
+/**********
+ * Memory
+ **********/
+ //control
+ wire mem_memWrite;
+ wire mem_memIsSigned;
+ wire [1:0] mem_memDataSize;
+
+ //mem
+ wire [STATUS_WIDTH-1:0] mem_status;
+ wire [BIT_WIDTH-1:0] mem_ALUDataOut;
+ wire [BIT_WIDTH-1:0] mem_regData;
+ wire mem_C, mem_Z, mem_S, mem_V;
+
+/**********
+ * writeback
+ **********/
+
+	wire [BIT_WIDTH-1:0] wb_dataMemOut;
+	wire [BIT_WIDTH-1:0] wb_out;
+	wire [BIT_WIDTH-1:0] wb_ALUDataOut;
+
+	wire [REG_ADDR_WIDTH-1:0] wb_regToWrite;
+
+/**********
+ * Glue Logic
+ **********/
+
  //instruction splitting
- assign opcode = instruction[31:26];
- assign rs = instruction[25:21];
- assign rt = instruction[20:16];
- assign rd = instruction[15:11];
- assign shamt = instruction[10:6];
- assign funct = instruction[5:0];
- assign immediate = instruction[15:0];
- assign jConst    = instruction[25:0];
- 
+ assign id_opcode = id_instruction[31:26];
+ assign id_rs = id_instruction[25:21];
+ assign id_rt = id_instruction[20:16];
+ assign id_rd = id_instruction[15:11];
+ assign id_shamt = id_instruction[10:6];
+ assign id_funct = id_instruction[5:0];
+ assign id_immediate = id_instruction[15:0];
+ assign id_jConst    = id_instruction[25:0];
+
+ //branching
+ assign id_reg_eq = id_readData[0] == id_readData[1];
+ assign branchTaken = (eq && branch && id_reg_eq) || (!eq && branch && !id_reg_eq);
+
  //renaming
- assign ALUDataIn[0] = readData[0];
- assign branchTaken = (eq && branch && Z) || (!eq && branch && !Z);
- assign C = status[3];
- assign Z = status[2];
- assign S = status[1];
- assign V = status[0];
- 
+ assign ex_ALUDataIn[0] = ex_readData[0];
+ assign mem_C = mem_status[3];
+ assign mem_Z = mem_status[2];
+ assign mem_S = mem_status[1];
+ assign mem_V = mem_status[0];
+
  //register renaming
- assign regRead[1] = rt;
- assign regRead[0] = rs;
- 
+ assign id_regRead[1] = id_rt;
+ assign id_regRead[0] = id_rs;
+
  //array packing
- `PACK_ARRAY(REG_ADDR_WIDTH, 2, regRead, rr_tmp, U_BLK_0, idx_0)
- `UNPACK_ARRAY(BIT_WIDTH, 2, readData, q_tmp, U_BLK_1, idx_1)
- 
- 
- `PACK_ARRAY(BIT_WIDTH, 2, ALUDataIn, ALUDataIn_tmp, U_BLK_2, idx_2)
- 
+ `PACK_ARRAY(REG_ADDR_WIDTH, 2, id_regRead, id_rr_tmp, U_BLK_0, idx_0)
+ `UNPACK_ARRAY(BIT_WIDTH, 2, id_readData, id_q_tmp, U_BLK_1, idx_1)
+
+
+ `PACK_ARRAY(BIT_WIDTH, 2, ex_ALUDataIn, ex_ALUDataIn_tmp, U_BLK_2, idx_2)
+
 /**********
  * Synchronous Logic
  **********/
- 
- //replace
- always @(posedge clk_sys) begin
-	if(rst) begin
-		PC <= 0;
-	end else begin
-		PC <= PC_in;
-	end
- end
- 
 /**********
- * Glue Logic 
+ * Glue Logic
  **********/
- assign PC_out = PC; //replace
  assign clk_mem = clk;
  assign clk_sys = clk;
 /**********
  * Components
  **********/
- 
+
  /**********
  * Fetch
  **********/
- 
+
+ 	delay #(
+		.BIT_WIDTH(BIT_WIDTH),
+		.DEPTH(1),
+		.DELAY(1),
+		.ARCH_SEL(0)
+	)U_PC(
+		.clk(clk),
+		.rst(rst),
+		.en_n(1'b0),
+		.dataIn(PC_in),
+		.dataOut(PC_out)
+	);
+
    adder #(
 		.BIT_WIDTH(BIT_WIDTH),
 		.DELAY(DELAY),
@@ -189,21 +240,22 @@ module datapath_r0 #(
 		.inB(4),
 		.out({PC_plus4_ovf, PC_plus4})
 	);
- 
+
+ //address has register (uses that instead of normal PC)
  programMem U_PROGRAM_MEMORY(
-		.address(PC_out[7:2]),
+		.address(PC_in[7:2]),
 		.clock(clk_mem),
-		.q(instruction)
+		.q(id_instruction) //output has register
 	);
- 
+
  /**********
  * IF/ID
  **********/
- 
+
  /**********
  * Decode
  **********/
- 
+
   //controller stuff
   	controller #(
 		.OP_WIDTH(OP_WIDTH),
@@ -213,30 +265,30 @@ module datapath_r0 #(
 	)U_CONTROLLER(
 		.clk(clk_sys),
 		.rst(rst),
-		.opcode(opcode),
+		.opcode(id_opcode),
 		.ALUop(ALUop),
 		.regWrite(regWrite),
 	   .regDest(regDest),
 	   .memToReg(memToReg),
-		
+
 		.load_upper(load_upper),
 	   .isSigned(isSigned),
 		.ALUsrc(ALUsrc),
-	
+
 		.jump(jump),
 		.jal(jal),
 		.branch(branch),
 		.eq(eq),
-	
+
 		.memRead(memRead),
 		.memWrite(memWrite),
-		
+
 		.memIsSigned(memIsSigned),
 		.memDataSize(memDataSize),
-		
+
 		.combined(combined)
 	);
- 
+
   //register file stuff
 	 registerFile #(
 	  .DATA_WIDTH(BIT_WIDTH),
@@ -244,16 +296,16 @@ module datapath_r0 #(
 	  .REG_DEPTH(32),
 	  .ARCH_SEL(0)
 	)U_REGFILE(
-	  .clk(~clk_sys),
+	  .clk(clk_sys),
 	  .rst(rst),
 	  .jal(jal),
-	  .wr(regWrite),
-	  .rr(rr_tmp),
-	  .rw(regToWrite),
-	  .d(regWriteData),
-	  .q(q_tmp)
+	  .wr(wb_regWrite),
+	  .rr(id_rr_tmp),
+	  .rw(wb_regToWrite),
+	  .d(id_regWriteData),
+	  .q(id_q_tmp)
 	);
-	
+
 	mux #(
 	  .BIT_WIDTH(REG_ADDR_WIDTH),
 	  .DEPTH(2),
@@ -262,11 +314,11 @@ module datapath_r0 #(
 	  .clk(clk_sys),
 	  .rst(rst),
 	  .en_n(1'b0),
-	  .dataIn({rd, rt}),
+	  .dataIn({id_rd, id_rt}),
 	  .sel(regDest),
-	  .dataOut(regToWrite)
+	  .dataOut(id_regToWrite)
 	);
-	
+
 	//sign extender stuff
 	 sign_extend #(
 		.BIT_WIDTH_IN(IMM_WIDTH),
@@ -279,11 +331,11 @@ module datapath_r0 #(
 		.rst(rst),
 		.is_signed(isSigned),
 		.load_upper(load_upper),
-		.dataIn(immediate),
-		.dataOut(imm_extended)
+		.dataIn(id_immediate),
+		.dataOut(id_imm_extended)
 	);
-	
-	
+
+
 	//branch address calculation
 	adder #(
 		.BIT_WIDTH(BIT_WIDTH),
@@ -293,10 +345,10 @@ module datapath_r0 #(
 		.clk(clk_sys),
 		.rst(rst),
 		.inA(PC_out),
-		.inB({imm_extended[BIT_WIDTH-3:0], 2'b0}), //left shift by 2
+		.inB({id_imm_extended[BIT_WIDTH-3:0], 2'b0}), //left shift by 2
 		.out({PC_branchTmp_ovf, PC_branchTmp})
 	);
-	
+
 	mux #(
 	  .BIT_WIDTH(BIT_WIDTH),
 	  .DEPTH(2),
@@ -309,7 +361,7 @@ module datapath_r0 #(
 	  .sel(branchTaken),
 	  .dataOut(PC_branchMux)
 	);
-	
+
 	//picks between branch and jumps
 	mux #(
 	  .BIT_WIDTH(BIT_WIDTH),
@@ -320,16 +372,16 @@ module datapath_r0 #(
 	  .rst(rst),
 	  .en_n(1'b0),
 	  .dataIn({
-			{PC_out[31:28], jConst, 2'b0}, 
-			{PC_out[31:28], jConst, 2'b0}, 
-			ALUDataOut, PC_branchMux}),
+			{PC_out[31:28], id_jConst, 2'b0},
+			{PC_out[31:28], id_jConst, 2'b0},
+			mem_ALUDataOut, PC_branchMux}), //change
 	  .sel({jump || jal, jr}),
 	  .dataOut(PC_in)
 	);
- 
- 
 
-	
+
+
+
 	//for jal
 	mux #(
 	  .BIT_WIDTH(BIT_WIDTH),
@@ -339,22 +391,48 @@ module datapath_r0 #(
 	  .clk(clk_sys),
 	  .rst(rst),
 	  .en_n(1'b0),
-	  .dataIn({PC_plus4 + 4, wbOut}),
+	  .dataIn({PC_plus4 + 4, wb_out}),
 	  .sel(jal),
-	  .dataOut(regWriteData)
+	  .dataOut(id_regWriteData)
 	);
- 
- 
+
+
  /**********
  * ID/EX
  **********/
- 
+
+ delay #(
+	 .BIT_WIDTH(ALUOP_WIDTH + FUNCT_WIDTH + SHAMT_WIDTH + 1),
+	 .DEPTH(1),
+	 .DELAY(1),
+	 .ARCH_SEL(0)
+ )U_ID_EX_CONTROL_DELAY(
+	 .clk(clk),
+	 .rst(rst),
+	 .en_n(1'b0),
+	 .dataIn({id_funct, id_shamt, ALUop, ALUsrc}),
+	 .dataOut({ex_funct, ex_shamt, ex_ALUop, ex_ALUsrc})
+ );
+
+ delay #(
+	 .BIT_WIDTH(BIT_WIDTH),
+	 .DEPTH(3),
+	 .DELAY(1),
+	 .ARCH_SEL(0)
+ )U_ID_EX_IMM_DELAY(
+	 .clk(clk),
+	 .rst(rst),
+	 .en_n(1'b0),
+	 .dataIn({id_imm_extended, id_readData[1], id_readData[0]}),
+	 .dataOut({ex_imm_extended, ex_readData[1], ex_readData[0]})
+ );
+
  /**********
  * Execute
  **********/
- 
+
  //ALU stuff
-	
+
 	 ALU_controller #(
 		.FUNCT_WIDTH(FUNCT_WIDTH),
 		.ALUOP_WIDTH(ALUOP_WIDTH),
@@ -364,12 +442,12 @@ module datapath_r0 #(
 	)U_ALU_CONTROLLER(
 		.clk(clk_sys),
 		.rst(rst),
-		.funct(funct),
-		.ALUop(ALUop),
-		.ALUfunct(ALUfunct),
+		.funct(ex_funct),
+		.ALUop(ex_ALUop),
+		.ALUfunct(ex_ALUfunct),
 		.jr(jr)
 	);
-	
+
 	mux #(
 	  .BIT_WIDTH(BIT_WIDTH),
 	  .DEPTH(2),
@@ -378,54 +456,106 @@ module datapath_r0 #(
 	  .clk(clk_sys),
 	  .rst(rst),
 	  .en_n(1'b0),
-	  .dataIn({imm_extended, readData[1]}),
-	  .sel(ALUsrc),
-	  .dataOut(ALUDataIn[1])
+	  .dataIn({ex_imm_extended, ex_readData[1]}),
+	  .sel(ex_ALUsrc),
+	  .dataOut(ex_ALUDataIn[1])
 	);
-	
+
 	 alu #(
 		.DATA_WIDTH(BIT_WIDTH),
 		.CTRL_WIDTH(ALUFUNCT_WIDTH),
 		.STATUS_WIDTH(STATUS_WIDTH),
 		.SHAMT_WIDTH(SHAMT_WIDTH),
-		.DELAY(DELAY),
+		.DELAY(1),
 		.ARCH_SEL(0)
 	)U_ALU(
 		.clk(clk_sys),
 		.rst(rst),
-		.dataIn(ALUDataIn_tmp),
-		.ctrl(ALUfunct),
-		.shamt(shamt), 
-		.dataOut(ALUDataOut),
-		.status(status)
+		.dataIn(ex_ALUDataIn_tmp),
+		.ctrl(ex_ALUfunct),
+		.shamt(ex_shamt),
+		.dataOut(mem_ALUDataOut),
+		.status(mem_status)
 	);
- 
+
  /**********
  * EX/MEM
  **********/
- 
+
+ delay #(
+	.BIT_WIDTH(BIT_WIDTH),
+	.DEPTH(1),
+	.DELAY(1),
+	.ARCH_SEL(0)
+ )U_EX_MEM_REG_DATA_DELAY(
+	.clk(clk),
+	.rst(rst),
+	.en_n(1'b0),
+	.dataIn(ex_readData[1]),
+	.dataOut(mem_regData)
+ );
+
+ delay #(
+	.BIT_WIDTH(4),
+	.DEPTH(1),
+	.DELAY(2),
+	.ARCH_SEL(0)
+ )U_ID_MEM_CONTROL_DELAY(
+	.clk(clk),
+	.rst(rst),
+	.en_n(1'b0),
+	.dataIn({memWrite, memIsSigned, memDataSize}),
+	.dataOut({mem_memWrite, mem_memIsSigned, mem_memDataSize})
+ );
+
  /**********
  * Memory
  **********/
- 
+
  	dataRAM U_DATA_MEMORY(
 		.clk(clk_mem),
-		.data(readData[1]),
-		.addr(ALUDataOut[7:0]), 
-		.wren(memWrite),
-		.isSigned(memIsSigned),
-		.dataSize(memDataSize),
-		.q(dataMemOut)
+		.data(mem_regData),
+		.addr(mem_ALUDataOut[7:0]),
+		.wren(mem_memWrite),
+		.isSigned(mem_memIsSigned),
+		.dataSize(mem_memDataSize),
+		.q(wb_dataMemOut)
 	);
- 
+
  /**********
  * MEM/WB
  **********/
- 
+
+ delay #(
+	.BIT_WIDTH(BIT_WIDTH),
+	.DEPTH(1),
+	.DELAY(1),
+	.ARCH_SEL(0)
+ )U_MEM_WB_ALUOUT_DELAY(
+	.clk(clk),
+	.rst(rst),
+	.en_n(1'b0),
+	.dataIn(mem_ALUDataOut),
+	.dataOut(wb_ALUDataOut)
+ );
+
+ delay #(
+ .BIT_WIDTH(REG_ADDR_WIDTH + 2),
+ .DEPTH(1),
+ .DELAY(3),
+ .ARCH_SEL(0)
+ )U_ID_WB_CONTROL_DELAY(
+ .clk(clk),
+ .rst(rst),
+ .en_n(1'b0),
+ .dataIn({memToReg, regWrite, id_regToWrite}),
+ .dataOut({wb_memToReg, wb_regWrite, wb_regToWrite})
+ );
+
  /**********
  * Write Back
  **********/
- 
+
  	mux #(
 	  .BIT_WIDTH(BIT_WIDTH),
 	  .DEPTH(2),
@@ -434,12 +564,12 @@ module datapath_r0 #(
 	  .clk(clk_sys),
 	  .rst(rst),
 	  .en_n(1'b0),
-	  .dataIn({dataMemOut, ALUDataOut}), //update
-	  .sel(memToReg),
-	  .dataOut(wbOut)
+	  .dataIn({wb_dataMemOut, wb_ALUDataOut}), //update
+	  .sel(wb_memToReg),
+	  .dataOut(wb_out)
 	);
 
- 
+
 /**********
  * Output Combinatorial Logic
  **********/
